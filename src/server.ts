1258440eb6a91596c39ad373d141beba5320759c
Rodrigo Monteiro <rp.monteiro130@gmail.com>
2020-06-23 00:40:02 -0300

    Padroes EditorConfig / Prettier / Eslint

--- src/server.ts
+++ src/server.ts
@@ -1,11 +1,17 @@
 import express from 'express';
+// src/server.ts
+
+import routes from './routes';
 
 const app = express();
 
+// eslint-disable-next-line arrow-body-style
 app.get('/', (request, response) => {
-    return response.json({ message: 'Hello '})
-})
+  return response.json({
+    message: 'Hello',
+  });
+});
 
-app.listen(3333 , () => {
-    console.log('🐱‍🏍🐱‍🏍 - Server started!' );
+app.listen(3333, () => {
+  console.log('🐱‍🏍🐱‍🏍 - Server started!');
 });
