086ff1e46c5823e80a6e8d0c30b6cfe846a707a0
Rodrigo Monteiro <rp.monteiro130@gmail.com>
2020-06-23 22:01:23 -0300

    Aula: Model de Agendamento(appointments)

--- src/routes/appointments.routes.ts
+++ src/routes/appointments.routes.ts
@@ -1,14 +1,9 @@
 import { Router } from 'express';
 import { uuid } from 'uuidv4';
 import { startOfHour, parseISO, isEqual } from 'date-fns';
+import Appointment from '../models/Appointments';
 
 const appoitmentsRouter = Router();
-
-interface Appointment {
-  id: string;
-  provider: string;
-  date: Date;
-}
 
 const appointments: Appointment[] = [];
 
@@ -27,11 +22,8 @@ appoitmentsRouter.post('/', (request, response) => {
       .json({ message: 'This appointment is already booked' });
   }
 
-  const appointment = {
-    id: uuid(),
-    provider,
-    date: parsedDate,
-  };
+  // instanciacao
+  const appointment = new Appointment(provider, parsedDate);
 
   appointments.push(appointment);
 
