026c2cad827a70e236673eea2b2b6d02a0afa9a8
Rodrigo Monteiro <rp.monteiro130@gmail.com>
2020-06-27 17:26:41 -0300

    Consegui corrigir os Migrations!!!!

---
+++ src/models/User.ts
@@ -0,0 +1,34 @@
+/* eslint-disable camelcase */
+import {
+  Entity,
+  Column,
+  PrimaryGeneratedColumn,
+  CreateDateColumn,
+  UpdateDateColumn,
+} from 'typeorm';
+
+// Alterar tsconfig.json a propriedade strictPropertyInicialization: false
+
+// KISS - KEEP IF SIMPLE & STUPID
+
+@Entity('users')
+class User {
+  @PrimaryGeneratedColumn('uuid')
+  id: string;
+
+  @Column()
+  name: string;
+
+  @Column()
+  email: string;
+
+  @Column()
+  password: string;
+
+  @CreateDateColumn()
+  created_at: Date;
+
+  @UpdateDateColumn()
+  updated_at: Date;
+}
+export default User;
