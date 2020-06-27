a15f5e04ca4b18045dab6c5b08e2c0b76e37f13e
Rodrigo Monteiro <rp.monteiro130@gmail.com>
2020-06-27 17:07:24 -0300

    Gerado padrão da tabela Appointments

---
+++ src/database/migrations/1593287143816-AlterAppointmentsCreateAt.ts
@@ -0,0 +1,19 @@
+import { MigrationInterface, QueryRunner, TableColumn } from 'typeorm';
+
+export default class AlterAppointmentsCreateAt1593287143816
+  implements MigrationInterface {
+  public async up(queryRunner: QueryRunner): Promise<void> {
+    await queryRunner.addColumn(
+      'appointments',
+      new TableColumn({
+        name: 'created_at',
+        type: 'timestamp',
+        default: 'now()',
+      }),
+    );
+  }
+
+  public async down(queryRunner: QueryRunner): Promise<void> {
+    await queryRunner.dropColumn('appointments', 'created_at');
+  }
+}
---
+++ src/database/migrations/1593287282524-AlterAppointmentsAlterColumnId.ts
@@ -0,0 +1,72 @@
+import { MigrationInterface, QueryRunner, TableColumn, Table } from 'typeorm';
+
+export default class AlterAppointmentsAlterColumnId1593287282524
+  implements MigrationInterface {
+  public async up(queryRunner: QueryRunner): Promise<void> {
+    await queryRunner.dropTable('appointments');
+
+    await queryRunner.createTable(
+      new Table({
+        name: 'appointments',
+        columns: [
+          {
+            name: 'id',
+            type: 'uuid',
+            isPrimary: true,
+            generationStrategy: 'uuid',
+            default: 'uuid_generate_v4()',
+          },
+          {
+            name: 'provider',
+            type: 'varchar',
+            isNullable: false,
+          },
+          {
+            name: 'date',
+            type: 'timestamp with time zone',
+            isNullable: false,
+          },
+          {
+            name: 'created_at',
+            type: 'timestamp',
+            default: 'now()',
+          },
+          {
+            name: 'updated_at',
+            type: 'timestamp',
+            default: 'now()',
+          },
+        ],
+      }),
+    );
+  }
+
+  public async down(queryRunner: QueryRunner): Promise<void> {
+    await queryRunner.dropTable('appointments');
+
+    await queryRunner.createTable(
+      new Table({
+        name: 'appointments',
+        columns: [
+          {
+            name: 'id',
+            type: 'varchar',
+            isPrimary: true,
+            generationStrategy: 'uuid',
+            default: 'uuid_generate_v4()',
+          },
+          {
+            name: 'provider',
+            type: 'varchar',
+            isNullable: false,
+          },
+          {
+            name: 'date',
+            type: 'timestamp with time zone',
+            isNullable: false,
+          },
+        ],
+      }),
+    );
+  }
+}
